465e32f93116b6307b96f552f02af5d1b5111707
Williamlundberg10 <williamlundberg10.home@gmail.com>
2025-09-01 13:56:46 +0200

    Update service-worker.js

--- web/pwa-app/service-worker.js
+++ web/pwa-app/service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = "pwa-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/manifest.json",
-  "/icons/s.png"
+  "/william-w.se/web/pwa-app/",
+  "/william-w.se/web/pwa-app/index.html",
+  "/william-w.se/web/pwa-app//styles.css",
+  "/william-w.se/web/pwa-app//script.js",
+  "/william-w.se/web/pwa-app//manifest.json",
+  "/william-w.se/web/pwa-app//icons/s.png"
 ];
 
 // Install Service Worker & Cache Files
@@ -40,3 +40,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
